e438c01c28118e1d554950c411bf6cedf399be8c
ranjithsnair <ranjithsnair@live.com>
2019-12-11 10:18:33 +0800

    Update dataImport.py

--- dataImport.py
+++ dataImport.py
@@ -5,11 +5,15 @@ username = 'nhfpgoiwhzrekw'
 password = 'e2773b491671c7f91f561c94c665a98586fef404eef9e7884d48e5d43a11ac59'
 database = 'dcsjh104da15vj'
 
-# Simple routine to run a query on a database and print the results:
 def doQuery(conn) :
+    print('Inside doQuery')
     cur = conn.cursor()
 
     cur.execute('CREATE TABLE "receipts" (ministry_department_state varchar,total_receipts integer,total_disposal integer, total_pending integer,pending_more_than_1_year integer,pending_between_6_to_12_months integer,pending_between_2_to_6_months integer,pending_less_than_2_months integer);')
+    print('Completed create table')
+    cur.close()
+    conn.commit()
+    print('Completed')
 
 import psycopg2
 myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
