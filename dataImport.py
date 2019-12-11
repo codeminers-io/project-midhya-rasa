121cf090bb154de3465d273208d86580eb281ac9
ranjithsnair <ranjithsnair@live.com>
2019-12-11 15:48:42 +0800

    Update dataImport.py

--- dataImport.py
+++ dataImport.py
@@ -15,31 +15,16 @@ def doQuery(conn, query) :
 import psycopg2
 import csv
 
-with open('/workdir/dataset/4.csv', encoding='utf-8') as csv_file:
+with open('/workdir/dataset/NodalOfficer_Details.csv', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count > 0 and line_count < 4000:
+        if line_count > 0 and line_count <= 5000:
             myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-            doQuery(myConnection, 'INSERT INTO \"receipts\" (ministry_department_state,total_receipts,total_disposal, total_pending,pending_more_than_1_year,pending_between_6_to_12_months,pending_between_2_to_6_months,pending_less_than_2_months) VALUES (\'' + row[0] + '\',\'' + row[1] + '\', \'' + row[2] + '\', \'' + row[3] + '\', \'' + row[4] + '\', \'' + row[5] + '\', \'' + row[6] + '\', \'' + row[7] + '\');')
+            doQuery(myConnection, 'INSERT INTO \"nodal_officer_details\" (apex_ministry_dept_state,parent_of_organisation,org_code,org_name,contact_address1,contact_address2,contact_address3,pincode,pg_officer_desig,organisation_levels) VALUES (\'' + row[0] + '\',\'' + row[1] + '\', \'' + row[2] + '\', \'' + row[3] + '\', \'' + row[4] + '\', \'' + row[5] + '\', \'' + row[6] + '\', \'' + row[7] + '\',\'' + row[8] + '\',\'' + row[9] + '\');')
             myConnection.close()
             print(f'Processing {line_count}')
             
         line_count += 1
     
     print(f'Processed {line_count} lines.')
-    
-with open('/workdir/dataset/3.csv', encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count > 0 and line_count < 4000:
-            myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-            doQuery(myConnection, 'INSERT INTO \"receipts_history\" (org_name,\"year\",\"month\",recetpts,disposals) VALUES (\'' + row[0] + '\',\'' + row[1] + '\', \'' + row[2] + '\', \'' + row[3] + '\', \'' + row[4] + '\');')
-            myConnection.close()
-            print(f'Processing {line_count}')
-            
-        line_count += 1
-    
-    print(f'Processed {line_count} lines.')
-
