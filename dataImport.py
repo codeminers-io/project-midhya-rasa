c48a43f97c224ff572cea2e307cc93bc1021f804
ranjithsnair <ranjithsnair@live.com>
2019-12-11 10:39:31 +0800

    Update dataImport.py

--- dataImport.py
+++ dataImport.py
@@ -9,8 +9,11 @@ def doQuery(conn) :
     print('Inside doQuery')
     cur = conn.cursor()
 
-    cur.execute('CREATE TABLE "receipts" (ministry_department_state varchar,total_receipts integer,total_disposal integer, total_pending integer,pending_more_than_1_year integer,pending_between_6_to_12_months integer,pending_between_2_to_6_months integer,pending_less_than_2_months integer);')
-    print('Completed create table')
+    cur.execute('CREATE TABLE "nodal_officer_details" (apex_ministry_dept_state varchar,parent_of_organisation varchar,org_code varchar,org_name varchar,contact_address1 varchar,contact_address2 varchar,contact_address3 varchar,pincode varchar,pg_officer_desig varchar,organisation_levels smallint);')
+    print('Completed create table1')
+    
+    cur.execute('CREATE TABLE "receipts_history" (org_name varchar,year integer,month integer,recetpts integer,disposals integer);')
+    print('Completed create table2')
     cur.close()
     conn.commit()
     print('Completed')
