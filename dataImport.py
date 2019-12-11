8c09413314b0db1affb87f0ac0f05c12a3356fe1
ranjithsnair <ranjithsnair@live.com>
2019-12-11 10:55:49 +0800

    Update dataImport.py

--- dataImport.py
+++ dataImport.py
@@ -5,20 +5,25 @@ username = 'nhfpgoiwhzrekw'
 password = 'e2773b491671c7f91f561c94c665a98586fef404eef9e7884d48e5d43a11ac59'
 database = 'dcsjh104da15vj'
 
-def doQuery(conn) :
-    print('Inside doQuery')
+# Simple routine to run a query on a database and print the results:
+def doQuery(conn, query) :
     cur = conn.cursor()
 
-    cur.execute('CREATE TABLE "nodal_officer_details" (apex_ministry_dept_state varchar,parent_of_organisation varchar,org_code varchar,org_name varchar,contact_address1 varchar,contact_address2 varchar,contact_address3 varchar,pincode varchar,pg_officer_desig varchar,organisation_levels smallint);')
-    print('Completed create table1')
-    
-    cur.execute('CREATE TABLE "receipts_history" (org_name varchar,year integer,month integer,recetpts integer,disposals integer);')
-    print('Completed create table2')
-    cur.close()
-    conn.commit()
-    print('Completed')
+    cur.execute(query)
 
 import psycopg2
-myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-doQuery(myConnection)
-myConnection.close()
+import csv
+
+with open('~/dataset/4.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count < 3000:
+            myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+            doQuery(myConnection, 'INSERT INTO \"receipts\" (ministry_department_state varchar,total_receipts integer,total_disposal integer, total_pending integer,pending_more_than_1_year integer,pending_between_6_to_12_months integer,pending_between_2_to_6_months integer,pending_less_than_2_months integer) VALUES (\'' + row[0] + '\',\'' + row[1] + '\', \'' + row[2] + '\', \'' + row[3] + '\', \'' + row[4] + '\', \'' + row[5] + '\', \'' + row[6] + '\', \'' + row[7] + '\');')
+            myConnection.close()
+            print(f'Processing {line_count}')
+            
+        line_count += 1
+    
+    print(f'Processed {line_count} lines.')
